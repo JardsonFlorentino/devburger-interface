3edd9aee9acfefe9be3534c3f52beade278cb550
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-11-28 23:31:10 -0300

    modificacaoVercel

--- src/components/CategoryCarousel/index.jsx
+++ src/components/CategoryCarousel/index.jsx
@@ -22,22 +22,10 @@ export function CategoryCarousel() {
     }, []);
 
     const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 4,
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1280 },
-            items: 4,
-        },
-        tablet: {
-            breakpoint: { max: 1280, min: 690 },
-            items: 3,
-        },
-        mobile: {
-            breakpoint: { max: 690, min: 0 },
-            items: 2,
-        },
+        superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 4 },
+        desktop: { breakpoint: { max: 3000, min: 1280 }, items: 4 },
+        tablet: { breakpoint: { max: 1280, min: 690 }, items: 3 },
+        mobile: { breakpoint: { max: 690, min: 0 }, items: 2 },
     };
 
     return (
