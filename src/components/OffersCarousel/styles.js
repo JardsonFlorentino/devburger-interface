f9792b096338cc01a22d06cc76313e2edbf10c5a
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-11-29 13:12:28 -0300

    ajustesResponsico2

--- src/components/OffersCarousel/styles.js
+++ src/components/OffersCarousel/styles.js
@@ -1,50 +1,82 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  overflow-x: hidden;
+
+  padding-left: 40px;
+  padding-bottom: 40px;
+
+  .carousel-item {
+    padding-right: 40px;
+  }
+
+  .react-multi-carousel-list {
+    overflow: visible;
+  }
+
+  /* Ajuste das setas */
+  .react-multiple-carousel__arrow {
+    width: 28px;
+    height: 28px;
+    min-width: 28px;
+  }
+
+  .react-multiple-carousel__arrow--left {
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .react-multiple-carousel__arrow--right {
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  /* MOBILE */
+  @media (max-width: 480px) {
+    padding-left: 15px;
+    padding-bottom: 30px;
+
     .carousel-item {
-        padding-right: 40px;
+      padding-right: 20px;
     }
 
-    overflow-x: hidden;
-
-    .react-multi-carousel-list {
-        overflow: visible;
+    .react-multiple-carousel__arrow {
+      width: 22px;
+      height: 22px;
+      min-width: 22px;
     }
-
-    .react-multiple-carousel__arrow--left {
-        left: 15px;
-        top: 10px;
-    }
-
-    .react-multiple-carousel__arrow--right {        
-        top: 10px;
-    }
-
-    padding-left: 40px;
-    padding-bottom: 40px;
-
-`
+  }
+`;
 
 export const Title = styled.h2`
-    font-size: 32px;
-    font-weight: 800;
-    color: ${(props) => props.theme.gren};
-    padding-bottom: 12px;
-    position: relative;
-    text-align: center;
-    margin: 70px 0;
+  font-size: 32px;
+  font-weight: 800;
+  color: ${(props) => props.theme.gren};
+
+  padding-bottom: 12px;
+  position: relative;
+  text-align: center;
+  margin: 60px 0 40px 0;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 56px;
+    height: 4px;
+    background-color: ${(props) => props.theme.gren};
+    left: calc(50% - 28px);
+  }
+
+  @media (max-width: 480px) {
+    font-size: 26px;
+    margin: 40px 0 25px 0;
 
     &::after {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        width: 56px;
-        height: 4px;
-        background-color: ${(props) => props.theme.gren};
-        left: calc(50% - 28px);
+      width: 40px;
+      left: calc(50% - 20px);
     }
-`
-
-
-
-
+  }
+`;
